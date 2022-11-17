89acb65b11411750c10ed20d0b51167812dce4ca
Leonardo Lino <leonardomercurio@hotmail.com>
2022-11-16 23:56:20 -0300

    List Organizacoes e List Jogadores

---
+++ src/gui/ExibeJogador.java
@@ -0,0 +1,146 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package gui;
+
+import static actions.ContainsUserByMail.containsJogadorByMail;
+import static io.Salvar.salvar;
+import classes.Jogador;
+import classes.OurBattle;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Windows
+ */
+public class ExibeJogador extends javax.swing.JFrame {
+
+    Jogador jogador;
+    
+    public ExibeJogador(Jogador jogador) {
+        this.jogador = jogador;
+        initComponents();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+        this.inicializaTela();
+    }
+
+    private void inicializaTela(){
+        if(this.jogador!= null){
+            this.jogador.getNome();
+            this.textNome.setText(this.jogador.getNome());
+            this.textUsername.setText(this.jogador.getUsername());
+            this.textEmail.setText(this.jogador.getEmail());
+            this.textPontuacao.setText(this.jogador.getPontuacao() + "");
+        }
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        textNome = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        textEmail = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        textPontuacao = new javax.swing.JPasswordField();
+        buttonCancelar = new javax.swing.JButton();
+        textUsername = new javax.swing.JTextField();
+        jLabel6 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel1.setText("Dados do Jogador");
+
+        jLabel2.setText("Nome:");
+
+        jLabel3.setText("E-mail:");
+
+        jLabel4.setText("Pontuação:");
+
+        buttonCancelar.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        buttonCancelar.setText("Voltar");
+        buttonCancelar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonCancelarActionPerformed(evt);
+            }
+        });
+
+        jLabel6.setText("Username:");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(buttonCancelar))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(textPontuacao)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel1)
+                                    .addComponent(textEmail, javax.swing.GroupLayout.PREFERRED_SIZE, 286, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(textNome, javax.swing.GroupLayout.PREFERRED_SIZE, 286, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 116, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 124, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(textUsername, javax.swing.GroupLayout.PREFERRED_SIZE, 286, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 139, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(0, 0, Short.MAX_VALUE)))))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(textNome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel3)
+                .addGap(1, 1, 1)
+                .addComponent(textEmail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel6)
+                .addGap(1, 1, 1)
+                .addComponent(textUsername, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(textPontuacao, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(buttonCancelar)
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void buttonCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonCancelarActionPerformed
+        this.dispose();
+    }//GEN-LAST:event_buttonCancelarActionPerformed
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton buttonCancelar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JTextField textEmail;
+    private javax.swing.JTextField textNome;
+    private javax.swing.JPasswordField textPontuacao;
+    private javax.swing.JTextField textUsername;
+    // End of variables declaration//GEN-END:variables
+}
--- src/gui/Organizacoes.java
+++ src/gui/Organizacoes.java
@@ -5,41 +5,26 @@
 package gui;
 
 import actions.AtualizaTabela;
-import actions.FindJogadorById;
 import classes.Jogador;
 import classes.OurBattle;
-import javax.swing.ListSelectionModel;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 
 /**
  *
  * @author Windows
  */
-public class Jogadores extends javax.swing.JFrame {
+public class Organizacoes extends javax.swing.JFrame {
 
     /**
      * Creates new form TorneiosOrganizador
      */
     OurBattle ourbattle; 
     Jogador jogador;
-    public Jogadores(OurBattle ourbattle) {
+    public Organizacoes(OurBattle ourbattle) {
         this.ourbattle = ourbattle;
         initComponents();
         this.setLocationRelativeTo(null);
         this.setVisible(true);
-        AtualizaTabela.atualizaTabela(this.ourbattle.getJogadores(), this.tabelaJogadores);
-    }
-    
-    private void iniciaTabela(){
-        this.tabelaJogadores.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        this.tabelaJogadores.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
-            @Override
-            public void valueChanged(ListSelectionEvent e) {
-                Jogador jogador = FindJogadorById.findJogadorById((String) tabelaJogadores.getValueAt(tabelaJogadores.getSelectedRow(), 0), ourbattle.getJogadores());
-                new ExibeJogador(jogador);
-            }
-        });
+        AtualizaTabela.atualizaTabela(this.ourbattle.getOrganizacoes(), this.tabelaOrganizacoes);
     }
     
     @SuppressWarnings("unchecked")
@@ -47,7 +32,7 @@ public class Jogadores extends javax.swing.JFrame {
     private void initComponents() {
 
         jScrollPane1 = new javax.swing.JScrollPane();
-        tabelaJogadores = new javax.swing.JTable();
+        tabelaOrganizacoes = new javax.swing.JTable();
         jButton2 = new javax.swing.JButton();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
@@ -57,33 +42,34 @@ public class Jogadores extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Gerenciar Torneios");
 
-        tabelaJogadores.setModel(new javax.swing.table.DefaultTableModel(
+        tabelaOrganizacoes.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
             },
             new String [] {
-                "ID", "Nome", "Username", "Pontuação"
+                "ID", "Razão Social", "Nome Fantasia", "E-mail", "CNPJ"
             }
         ) {
             boolean[] canEdit = new boolean [] {
-                false, false, false, false
+                false, false, false, false, false
             };
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
                 return canEdit [columnIndex];
             }
         });
-        tabelaJogadores.setColumnSelectionAllowed(true);
-        tabelaJogadores.getTableHeader().setReorderingAllowed(false);
-        jScrollPane1.setViewportView(tabelaJogadores);
-        tabelaJogadores.getColumnModel().getSelectionModel().setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
-        if (tabelaJogadores.getColumnModel().getColumnCount() > 0) {
-            tabelaJogadores.getColumnModel().getColumn(0).setMinWidth(0);
-            tabelaJogadores.getColumnModel().getColumn(0).setPreferredWidth(0);
-            tabelaJogadores.getColumnModel().getColumn(0).setMaxWidth(0);
-            tabelaJogadores.getColumnModel().getColumn(1).setResizable(false);
-            tabelaJogadores.getColumnModel().getColumn(2).setResizable(false);
-            tabelaJogadores.getColumnModel().getColumn(3).setResizable(false);
+        tabelaOrganizacoes.setColumnSelectionAllowed(true);
+        tabelaOrganizacoes.getTableHeader().setReorderingAllowed(false);
+        jScrollPane1.setViewportView(tabelaOrganizacoes);
+        tabelaOrganizacoes.getColumnModel().getSelectionModel().setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+        if (tabelaOrganizacoes.getColumnModel().getColumnCount() > 0) {
+            tabelaOrganizacoes.getColumnModel().getColumn(0).setMinWidth(0);
+            tabelaOrganizacoes.getColumnModel().getColumn(0).setPreferredWidth(0);
+            tabelaOrganizacoes.getColumnModel().getColumn(0).setMaxWidth(0);
+            tabelaOrganizacoes.getColumnModel().getColumn(1).setResizable(false);
+            tabelaOrganizacoes.getColumnModel().getColumn(2).setResizable(false);
+            tabelaOrganizacoes.getColumnModel().getColumn(3).setResizable(false);
+            tabelaOrganizacoes.getColumnModel().getColumn(4).setResizable(false);
         }
 
         jButton2.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
@@ -117,7 +103,7 @@ public class Jogadores extends javax.swing.JFrame {
         );
 
         jLabel4.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jLabel4.setText("Jogadores");
+        jLabel4.setText("Organizacoes");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -159,6 +145,6 @@ public class Jogadores extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField jTextField1;
-    private javax.swing.JTable tabelaJogadores;
+    private javax.swing.JTable tabelaOrganizacoes;
     // End of variables declaration//GEN-END:variables
 }
